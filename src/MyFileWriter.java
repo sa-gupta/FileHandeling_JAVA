079e16e71d4c7d9fb68138ef95538035987e713f
Sachin Gupta <sachin070gupta@gmail.com>
2021-03-22 23:56:54 +0530

    Create MyFileWriter.java

---
+++ src/MyFileWriter.java
@@ -0,0 +1,15 @@
+package com.cg.file;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Scanner;
+
+public class MyFileWriter {
+
+	public static void main(String[] args) {
+		File f = new File("output.txt");
+		Scanner scan = new Scanner(System.in);
+		
+	}
+
+}
